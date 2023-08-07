d3060b2013db0e1d1f7b7461cd3887c8bed61725
Manasse Daniel <danidkm242@gmail.com>
2023-08-07 23:40:39 +0100

    CFService  delete & findByCode methods impl

--- src/main/java/com/daniel/gestiondestock/services/implementation/CommandeFournisseurService.java
+++ src/main/java/com/daniel/gestiondestock/services/implementation/CommandeFournisseurService.java
@@ -1,22 +1,48 @@
 package com.daniel.gestiondestock.services.implementation;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.daniel.gestiondestock.dto.CommandeFournisseurDto;
+import com.daniel.gestiondestock.exception.EntityNotFoundException;
+import com.daniel.gestiondestock.exception.ErrorCodes;
+import com.daniel.gestiondestock.mapper.DtoMapper;
+import com.daniel.gestiondestock.model.CommandeFournisseur;
+import com.daniel.gestiondestock.repository.CommandeFournisseurRepository;
 import com.daniel.gestiondestock.services.contracts.ICommandeFournisseurService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
 public class CommandeFournisseurService implements ICommandeFournisseurService {
+
+  @Autowired
+  private CommandeFournisseurRepository repository;
 
   @Override
   public CommandeFournisseurDto findByCode(String code) {
-    
-    return null;
+    if (code == null) {
+      log.error("CommandeFournisseur with null id is not allowed");
+      return null;
+
+    }
+    Optional<CommandeFournisseur> commandeFournisseurOptional = this.repository.findByCode(code);
+    var commandeFournisseur = commandeFournisseurOptional.orElseThrow(
+        () -> new EntityNotFoundException("CommandeFournisseur not Found", ErrorCodes.COMMANDE_FOURNISSEUR_NOT_FOUND));
+    return DtoMapper.fromEntity(commandeFournisseur, CommandeFournisseurDto.class);
   }
 
   @Override
   public void delete(Integer id) {
-    // TODO Auto-generated method stub
-    
+    if (id == null) {
+      log.error("CommandeFournisseur with null id is not allowed");
+    }
+    this.repository.deleteById(id);
+
   }
 
   @Override
@@ -36,5 +62,5 @@ public class CommandeFournisseurService implements ICommandeFournisseurService {
     // TODO Auto-generated method stub
     return null;
   }
-  
+
 }
