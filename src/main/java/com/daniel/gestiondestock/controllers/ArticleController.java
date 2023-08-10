591f73d0e4596c79826f2c5deea90cc9d10d363d
Manasse Daniel <danidkm242@gmail.com>
2023-08-10 18:20:28 +0100

    swagger doc in article Controller

--- src/main/java/com/daniel/gestiondestock/controllers/ArticleController.java
+++ src/main/java/com/daniel/gestiondestock/controllers/ArticleController.java
@@ -8,18 +8,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.daniel.gestiondestock.controllers.api.ArticleApi;
 import com.daniel.gestiondestock.dto.ArticleDto;
+
 import com.daniel.gestiondestock.services.implementation.ArticleService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 import static com.daniel.gestiondestock.utils.Constants.APP_ROOT;
 
 @RestController
-@RequestMapping(APP_ROOT + "/article")
+@RequestMapping(APP_ROOT + "/articles")
 public class ArticleController implements ArticleApi {
 
   @Autowired
   private ArticleService articleService;
 
   @Override
+  @ApiOperation(value = "Delete a Article by ID", response = ArticleDto.class)
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "article found successfully and retrieved"),
+      @ApiResponse(code = 404, message = "Article does not exist")
+  })
   public void delete(Integer id) {
     articleService.delete(id);
   }
@@ -30,16 +40,30 @@ public class ArticleController implements ArticleApi {
   }
 
   @Override
+  @ApiOperation(value = "create a new article", response = ArticleDto.class)
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "article created successfully"),
+      @ApiResponse(code = 400, message = "articledto is not valid")
+  })
   public ArticleDto create(ArticleDto resource) {
     return articleService.save(resource);
   }
 
   @Override
+  @ApiOperation(value = "Get all articles", responseContainer = "List<ArticleDto.class>")
+  @ApiResponse(code = 200, message = "OK")
   public List<ArticleDto> getAll() {
     return articleService.findAll();
   }
 
   @Override
+  @ApiOperation(value = "Find an article by its ID", notes = "this methods allows you to retrieve an article by its ID", response = ArticleDto.class)
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "article found successfully and retrieved"),
+      @ApiResponse(code = 404, message = "Article does not exist")
+  }
+
+  )
   public ArticleDto getById(Integer id) {
     return articleService.findById(id);
 
--- src/main/java/com/daniel/gestiondestock/controllers/api/ArticleApi.java
+++ src/main/java/com/daniel/gestiondestock/controllers/api/ArticleApi.java
@@ -1,17 +1,25 @@
 package com.daniel.gestiondestock.controllers.api;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.http.MediaType;
 import com.daniel.gestiondestock.dto.ArticleDto;
+import static com.daniel.gestiondestock.utils.Constants.APP_ROOT;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
-public interface ArticleApi extends AbstractApi <ArticleDto> {
-    
-  @GetMapping(value ="/articles/{codeArticle}", produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(APP_ROOT + "/articles/")
+public interface ArticleApi extends AbstractApi<ArticleDto> {
+
+  @ApiOperation(value = "Find an article by its code", notes = "this methods allows you to retrieve an article by its code", response = ArticleDto.class)
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "article found successfully and retrieved"),
+      @ApiResponse(code = 404, message = "Article does not exist")
+  })
+  @GetMapping(value = "/articles/{codeArticle}", produces = MediaType.APPLICATION_JSON_VALUE)
   ArticleDto findByCodeArticle(String codeArticle);
- 
-  
 
 }
