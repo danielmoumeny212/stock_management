13274294436732ab7f23cf9f88338bea3f27d012
Manasse Daniel <danidkm242@gmail.com>
2023-08-10 16:52:06 +0100

    allowing any request from gestiondestock endpoint

---
+++ src/main/java/com/daniel/gestiondestock/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.daniel.gestiondestock.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig  {
+
+    
+    @Bean
+    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception{
+        http.securityMatcher("/gestiondestock/**")
+            .authorizeHttpRequests(authorize ->
+              authorize.anyRequest().anonymous());
+        
+        return http.build();
+        
+    }
+
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
